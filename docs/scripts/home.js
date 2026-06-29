2db629a42c23af5b3537049cab2d54f491d5b4a6
Adrian Bugarin <adrianbugarin30@gmail.com>
2026-06-28 18:33:57 -0700

    Fix feed API URL

--- docs/scripts/home.js
+++ docs/scripts/home.js
@@ -1,34 +1,4 @@
-fetch("https://clutchapp.onrender.com/clips")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-
-        const feed = document.getElementById("clipFeed");
-
-        data.clips.forEach(clip => {
-            const card = document.createElement("div");
-
-            card.innerHTML = `
-                <h3>${clip.username || "Unknown User"}</h3>
-                <p>${clip.caption}</p>
-                <p>Agent: ${clip.agent}</p>
-                <p>Rank: ${clip.rank}</p>
-
-                <video width="400" controls>
-                    <source src="https://clutchapp.onrender.com/${clip.video_path}" type="video/mp4">
-                </video>
-
-                <p id="likes-${clip.id}">Likes: ${clip.likes}</p>
-                <button onclick="likeClip(${clip.id})">
-                    Like
-                </button>
-
-                <hr>
-            `;
-
-            feed.appendChild(card);
-        });
-    });
+fetch("http://127.0.0.1:5000/clips")
     .then(response => response.json())
     .then(data => {
         console.log(data);
